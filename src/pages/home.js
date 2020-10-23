1691e7552f152c02937e03d975c09d4c39e80549
sourigna manibod <manibod1906@hotmail.com>
2020-10-22 23:06:50 -0400

    updated signin and firebase

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,36 +1,36 @@
-import React, { Fragment } from 'react';
-
-
+import React, { Fragment } from "react";
 
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
-import { HeaderContainer } from '../containers/header';
-import { Feature , Form } from '../components/';
+import { HeaderContainer } from "../containers/header";
+import { Feature, Form } from "../components/";
 
-
-export default function Home(){
-    return(
-        <Fragment>
-          <HeaderContainer>
-            <Feature>
-              <Feature.Title>
-                Unlimited films, TV programmes and more.
-              </Feature.Title>
-              <Feature.SubTitle>
-                Watch anywhere. Cancel at any time.
-              </Feature.SubTitle>
-              <Form>
-                <Form.Textbox placeholder="Enter your Email"></Form.Textbox>
-                <Form.Button>Sign in</Form.Button>
-                <Form.Break></Form.Break>
-                <Form.Text>Ready to watch? Enter your email to create or restart your membership.</Form.Text>
-              </Form>
-            </Feature>
-          </HeaderContainer>
-          <JumbotronContainer />
-          <FaqsContainer />
-          <FooterContainer />
-        </Fragment>
-    );
+export default function Home() {
+  return (
+    <Fragment>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <Form action="/sign-in">
+            <Form.Textbox placeholder="Enter your Email"></Form.Textbox>
+            <Form.Button type="submit">Sign in</Form.Button>
+            <Form.Break></Form.Break>
+            <Form.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </Form.Text>
+          </Form>
+        </Feature>
+      </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </Fragment>
+  );
 }
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,54 +1,78 @@
-import React, { Fragment, useState, useContext  } from 'react';
+import React, { Fragment, useState, useContext } from "react";
 
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 
-import { useHistory } from 'react-router-dom';
-import { FirebaseContext } from '../context/firebase';
-
-import { HeaderContainer } from '../containers/header';
+import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
-import { SignForm } from '../components'
-import * as ROUTES from '../constants/routes';
+import { SignForm } from "../components";
+import * as ROUTES from "../constants/routes";
 
-export default function Signin(){
+export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const history = useHistory();
-    const { firebase } = useContext(FirebaseContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [error,setError] = useState('');
+  const isInvalid = password === "" || email === "";
 
-    const isInvalid = password === '' || email === '';
+  const handleSignin = (event) => {
+    event.preventDefault();
 
-    const handleSignin = (event) => {
-        event.preventDefault();
-    
-        return firebase
-          .auth()
-          .signInWithEmailAndPassword(email, password)
-          .then(() => {
-            history.push(ROUTES.BROWSE);
-          })
-          .catch((error) => {
-            setEmail('');
-            setPassword('');
-            setError(error.message);
-          });
-      };
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmail("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
 
-    return <Fragment>
-            <HeaderContainer>
-                <SignForm>
-                    <SignForm.Title>Sign In</SignForm.Title>
-                    {error && <SignForm.Error>{error}</SignForm.Error>}
-                    <SignForm.Form onSubmit={handleSignin} method="POST">
-                        <SignForm.Input placeholder='Email' value={email} type='email' onChange={({target}) => setEmail(target.value)} />
-                        <SignForm.Input type='password' value={password} placeholder='Password' autoComplete='off' onChange={({ target }) => setPassword(target.value)} />
-                        <SignForm.Button  disabled={isInvalid} type="submit" data-testid="sign-in">Sign In</SignForm.Button>
-                    </SignForm.Form>
-                    <SignForm.Text>New to Netflix? <SignForm.Link to="/sign-up">Sign up now.</SignForm.Link></SignForm.Text>
-                </SignForm>
-            </HeaderContainer>
-            <FooterContainer />
-        </Fragment>;
+  return (
+    <Fragment>
+      <HeaderContainer>
+        <SignForm>
+          <SignForm.Title>Sign In</SignForm.Title>
+          {error && <SignForm.Error>{error}</SignForm.Error>}
+          <SignForm.Form onSubmit={handleSignin} method="POST">
+            <SignForm.Input
+              placeholder="Email"
+              value={email}
+              type="email"
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <SignForm.Input
+              type="password"
+              value={password}
+              placeholder="Password"
+              autoComplete="off"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <SignForm.Button
+              disabled={isInvalid}
+              type="submit"
+              data-testid="sign-in"
+            >
+              Sign In
+            </SignForm.Button>
+          </SignForm.Form>
+          <SignForm.Text>
+            New to Netflix?{" "}
+            <SignForm.Link to="/sign-up">Sign up now.</SignForm.Link>
+          </SignForm.Text>
+          <SignForm.Text>
+            --You can try the user: 'tester@email.com///123456' <br></br>to test
+            the app without creating a new user--{" "}
+          </SignForm.Text>
+        </SignForm>
+      </HeaderContainer>
+      <FooterContainer />
+    </Fragment>
+  );
 }
